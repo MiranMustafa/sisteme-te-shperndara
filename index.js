50368c44b50278b7ba975100ab56c6568480df4e
Miran Mustafa <miran@frakton.com>
2020-05-21 15:21:27 +0200

    minor fix

--- index.js
+++ index.js
@@ -3,39 +3,35 @@ const { Pool, Client } = require('pg')
 const http = require('http');
 
 const app = express();
-const server = http.Server(app);
+ const server = http.Server(app);
 const PORT = parseInt(process.env.PORT) || 5000;
-
-app.use(express.static('client'));
 
 server.listen(PORT, function(){
     console.log('Listening');
 });
 
 
-
-
 // pools will use environment variables
 // for connection information
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+// app.get('/db', async (req, res) => {
+//     try {
+//       const client = await pool.connect();
+//       const result = await client.query('SELECT * FROM test_table');
+//       const results = { 'results': (result) ? result.rows : null};
+//       res.render('pages/db', results );
+//       client.release();
+//     } catch (err) {
+//       console.error(err);
+//       res.send("Error " + err);
+//     }
+//   })
 // const pool = new Pool({
 //     user: 'postgres',
 //     host: 'localhost',

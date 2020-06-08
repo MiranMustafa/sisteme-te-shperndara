45f9bd5fbe3bd97cacc2cc3b0e589cc611d5a8a0
Miran Mustafa <miran@frakton.com>
2020-06-08 21:19:27 +0200

    added endpoints

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const { Pool, Client } = require('pg')
 const http = require('http');
 const bodyParser = require('body-parser');
+const io = require('socket.io')(http)
 const extension = '/api/v1';
+const connections = {}; // pairs of socket.id with username
 
 
 const crypto = require('crypto');
+
 
 const getHashedPassword = (password) => {
   const sha256 = crypto.createHash('sha256');
@@ -23,6 +26,275 @@ const PORT = parseInt(process.env.PORT) || 5000;
 
 server.listen(PORT, function () {
   console.log('Listening');
+});
+
+io.on('connection', (socket) => {
+  socket.on('new-user', username => {
+    connections[socket.id] = username;
+  });
+  socket.on('send-message-chat', data => {
+    data.userid;
+    data.chatid;
+    data.message;
+    data.timestamp;
+
+    const result = await client.query('SELECT * FROM chat where user_id_1 = $1 OR user_id_2  = $1 AND id = $2', [data.userid, data.chatid])[0];
+    let receiverId;
+
+    if (result.user_id1 !== data.userid) {
+      receiverId = result.user_id1;
+    }
+    if (result.user_id1 !== data.userid) {
+      receiverId = result.user_id2;
+    }
+
+    const username = await client.query('SELECT username from users where id = $1', [receiverId])[0].username;
+
+    receiverSocketId = getKeyByValue(username);
+
+    socket.to(receiverSocketId).emit('send-message', data);
+  });
+
+  function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+  }
+
+  socket.on('disconnect', () => {
+    socket.emit('disconnected');
+    delete connections[socket.id];
+  });
+  console.log('a user connected');
+  socket.emit('connected');
+});
+
+app.post(`${extension}/newAddress`, async (req, res) => {
+  console.log(req.body);
+  const { regiondId, municipalityId, neighborhood, streetId, objectId, latitude, longitude } = req.body;
+  if (!regiondId) {
+    return res.status(400).send({
+      success: false,
+      message: 'regionId is required'
+    });
+  }
+  if (!municipalityId) {
+    return res.status(400).send({
+      success: false,
+      message: 'municipalityId is required'
+    });
+  }
+  if (!neighborhood) {
+    return res.status(400).send({
+      success: false,
+      message: 'neighborhood is required'
+    });
+  }
+  if (!streetId) {
+    return res.status(400).send({
+      success: false,
+      message: 'streetId is required'
+    });
+  }
+  if (!objectId) {
+    return res.status(400).send({
+      success: false,
+      message: 'objectId is required'
+    });
+  }
+  if (!latitude) {
+    return res.status(400).send({
+      success: false,
+      message: 'latitude is required'
+    });
+  }
+  if (!longitude) {
+    return res.status(400).send({
+      success: false,
+      message: 'longitude is required'
+    });
+  }
+
+
+  const text = 'INSERT INTO address(region_id, municipality_id, neighborhood, street_id, object_id, latitude, longitude) VALUES ( $1, $2, $3, $4, $5, $6, $7);'
+  const values = [regiondId, municipalityId, neighborhood, streetId, objectId, latitude, longitude];
+  const client = await pool.connect();
+
+  try {
+    result = await client.query(text, values)
+    return res.status(200).send({
+      success: true,
+      message: 'added'
+    });
+  } catch (err) {
+    console.log(err.stack)
+    return res.status(500).send({
+      success: false,
+      message: 'failed'
+    })
+  }
+
+});
+
+app.post(`${extension}/newStreet`, async (req, res) => {
+  console.log(req.body);
+  const { municipalityId, country, code, name, type } = req.body;
+
+  if (!municipalityId) {
+    return res.status(400).send({
+      success: false,
+      message: 'municipalityId is required'
+    });
+  }
+  if (!country) {
+    return res.status(400).send({
+      success: false,
+      message: 'country is required'
+    });
+  }
+  if (!code) {
+    return res.status(400).send({
+      success: false,
+      message: 'code is required'
+    });
+  }
+  if (!name) {
+    return res.status(400).send({
+      success: false,
+      message: 'type is required'
+    });
+  }
+  if (!type) {
+    return res.status(400).send({
+      success: false,
+      message: 'type is required'
+    });
+  }
+
+  const text = 'INSERT INTO street( municipality_id, country, code, name, type) VALUES ( $1, $2, $3, $4, $5);'
+  const values = [municipalityId, country, code, name, type];
+  const client = await pool.connect();
+
+  try {
+    result = await client.query(text, values)
+    return res.status(200).send({
+      success: true,
+      message: 'added'
+    });
+  } catch (err) {
+    console.log(err.stack)
+    return res.status(500).send({
+      success: false,
+      message: 'failed'
+    })
+  }
+
+});
+
+app.post(`${extension}/newMunicipality`, async (req, res) => {
+  console.log(req.body);
+  const { name, postalCode } = req.body;
+  if (!name) {
+    return res.status(400).send({
+      success: false,
+      message: 'name is required'
+    });
+  }
+  if (!postalCode) {
+    return res.status(400).send({
+      success: false,
+      message: 'postalCode is required'
+    });
+  }
+
+  const text = 'INSERT INTO municipality(name, postal_code) VALUES ( $1, $2);'
+  const values = [name, postalCode];
+  const client = await pool.connect();
+
+  try {
+    result = await client.query(text, values)
+    return res.status(200).send({
+      success: true,
+      message: 'added'
+    });
+  } catch (err) {
+    console.log(err.stack)
+    return res.status(500).send({
+      success: false,
+      message: 'failed'
+    })
+  }
+
+});
+
+app.post(`${extension}/newObject`, async (req, res) => {
+  console.log(req.body);
+  const { type, numberOfFloors, numberOfEntrances } = req.body;
+  if (!type) {
+    return res.status(400).send({
+      success: false,
+      message: 'type is required'
+    });
+  }
+  if (!numberOfFloors) {
+    return res.status(400).send({
+      success: false,
+      message: 'numberOfFloors is required'
+    });
+  }
+  if (!numberOfEntrances) {
+    return res.status(400).send({
+      success: false,
+      message: 'numberOfEntrances is required'
+    });
+  }
+
+  const text = 'INSERT INTO address(type, number_of_floors, number_of_entrances) VALUES ( $1, $2, $3);'
+  const values = [type, numberOfFloors, numberOfEntrances];
+  const client = await pool.connect();
+
+  try {
+    result = await client.query(text, values)
+    return res.status(200).send({
+      success: true,
+      message: 'added'
+    });
+  } catch (err) {
+    console.log(err.stack)
+    return res.status(500).send({
+      success: false,
+      message: 'failed'
+    })
+  }
+
+});
+
+app.post(`${extension}/newRegion`, async (req, res) => {
+  console.log(req.body);
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).send({
+      success: false,
+      message: 'name is required'
+    });
+  }
+
+  const text = 'INSERT INTO region(name) VALUES ($1);'
+  const values = [name];
+  const client = await pool.connect();
+
+  try {
+    result = await client.query(text, values)
+    return res.status(200).send({
+      success: true,
+      message: 'added'
+    });
+  } catch (err) {
+    console.log(err.stack)
+    return res.status(500).send({
+      success: false,
+      message: 'failed'
+    })
+  }
+
 });
 
 app.post(`${extension}/login`, async (req, res) => {
@@ -65,7 +337,6 @@ app.post(`${extension}/login`, async (req, res) => {
 
 
 app.post(`${extension}/register`, async (req, res) => {
-  // console.log(req.body);
   const { email, password, name, surname, username, role } = req.body;
   if (!email) {
     return res.status(400).send({
@@ -144,34 +415,106 @@ app.post(`${extension}/register`, async (req, res) => {
 });
 
 app.get(`${extension}/getAdresses`, async (req, res) => {
-    console.log(req.query);
-    const client = await pool.connect();
-    let result = await client.query(
-    'SELECT address.id , address.neighborhood, address.latitude, address.longitude, region.name as region_name, '+
-    'street.country as street_country, street.code as street_code, street.type as street_type, '+ 
-    'object.type as object_type, object.number_of_floors as object_number_of_floors, object.number_of_entrances as object_number_of_entrances, '+ 
-    'municipality.name as municipality_name, municipality.postal_code as municipality_postal_code ' + 
-    'FROM address INNER JOIN region on region.id = address.region_id ' + 
-    'INNER JOIN street on street.id = address.street_id INNER JOIN object on object.id = address.object_id '+ 
+  console.log(req.query);
+  const client = await pool.connect();
+  let result = await client.query(
+    'SELECT address.id , address.neighborhood, address.latitude, address.longitude,region.id as region_id, region.name as region_name, ' +
+    'street.id as street_id, street.country as street_country, street.code as street_code, street.type as street_type, ' +
+    'object.id as object_id, object.type as object_type, object.number_of_floors as object_number_of_floors, object.number_of_entrances as object_number_of_entrances, ' +
+    'municipality.id as municipality_id,  municipality.name as municipality_name, municipality.postal_code as municipality_postal_code ' +
+    'FROM address INNER JOIN region on region.id = address.region_id ' +
+    'INNER JOIN street on street.id = address.street_id INNER JOIN object on object.id = address.object_id ' +
     'INNER JOIN municipality on municipality.id = address.municipality_id ; ');
-    res.status(200).send(
-      result.rows
-    );
+
+  res.status(200).send(
+    result.rows
+  );
 });
 
-//use for hosted db
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
+app.post(`${extension}/getChatsByUserId`, async (req, res) => {
+  /*
+  userid
+  query = "SELECT * x FROM chats where user_id_1 = userid OR user_id_2 = userid";
+  
+  */
+  const userId = req.body;
+  if (!userId) {
+    if (!email) {
+      return res.status(400).send({
+        success: false,
+        message: 'userId is required'
+      });
+    }
+  }
+
+  try {
+    const client = await pool.connect();
+    let result = await client.query("SELECT * FROM chat where user_id_1 = $1 OR user_id_2 = $1", [userId]);
+    return res.status(200).send({
+      success: true,
+      chats: result
+    });
+  } catch (err) {
+    console.log(err.stack)
+    return res.status(500).send({
+      success: false,
+      message: 'failed'
+    })
+  }
+
+});
+
+app.get(`${extension}/getMessagesByChatId`, async (req, res) => {
+  /**
+   * chatid
+   * return -> id, chatId , message, timestamp, senderId 
+   */
+  const chatId = req.body;
+  if (!userId) {
+    if (!email) {
+      return res.status(400).send({
+        success: false,
+        message: 'chatId is required'
+      });
+    }
+  }
+  const client = await pool.connect();
+
+  try {
+    let result = await client.query("SELECT * FROM chat_message where chat_id = $1", [chatId]);
+    return res.status(200).send({
+      success: true,
+      chats: result
+    });
+  } catch (err) {
+    console.log(err.stack)
+    return res.status(500).send({
+      success: false,
+      message: 'failed'
+    })
   }
 });
 
-//use for local db
+app.get(`${extension}/getUserInfoById`, async (req, res) => {
+
+  /** userid
+   *  query = "SELECT id, username from users where id = userid"
+   */
+});
+
+//use for hosted db
 // const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'mylocaldb',
-//   password: 'admin',
-//   port: 5432,
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
 // });
+
+//use for local db
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'mylocaldb',
+  password: 'admin',
+  port: 5432,
+});
